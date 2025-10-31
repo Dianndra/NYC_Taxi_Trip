ca9b0d6e3c87a29138cecdbab707b0157aa81152
Dianndra Oyemen <dianndraoyemhen@outlook.com>
2025-10-31 18:33:31 +0000

    Added img folder

--- get_data.py
+++ get_data.py
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import os
-import requests 
-
-#create data folder
-data_folder = "nyc_tripdata"
-os.makedirs(data_folder, exist_ok=True)
-
-def extract_data(year):
-    datasets = ["yellow", "green", "fhv", "fhvhv"]
-    months = range(1, 13)
-    
-    #loop each month in each dataset
-    for dataset in datasets:
-        for month in months:
-            month_number = f"{month:02d}"
-            url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{dataset}_tripdata_{year}-{month_number}.parquet"
-            filename = url.split("/")[-1]
-            local_path = os.path.join(data_folder, filename)
-
-            try:
-                response = requests.get(url, stream=True)
-                response.raise_for_status()
-
-                with open(local_path, "wb") as f:
-                    for chunk in response.iter_content(chunk_size=8192):
-                        f.write(chunk)
-            
-            except Exception as e:
-                print(f"error for {url}: {e}")
-
-    print("files downloaded successfully")
-
-extract_data(year=2024)
-
-=======
 import os
 import requests 
 import pyarrow.parquet as pq
@@ -68,5 +32,3 @@ def extract_data(year):
     print("files downloaded successfully")
 
 extract_data(year=2024)
-
->>>>>>> def9b2e (First commit)
--- script/full_load/dw_setup.sql
+++ script/full_load/dw_setup.sql
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
---create db
-CREATE OR REPLACE DATABASE nyc_trip_db;
-USE DATABASE nyc_trip_db;
-
---create staging area
-CREATE OR REPLACE STAGE nyc_trip_stage;
-
-
---create file format as parquet
-CREATE OR REPLACE FILE FORMAT parquet_format
-  TYPE = 'PARQUET';
-
-
-
---insert data into stage using snowinsight 
--- go to catalogue>Nyc_trip_db>public>stage
---double click on stage name and add file with the file button at top right
-
-list @public.nyc_trip_stage;
-
---create schema 
-CREATE OR REPLACE SCHEMA bronze;
-CREATE OR REPLACE SCHEMA silver;
-CREATE OR REPLACE SCHEMA gold;
-=======
 --create db
 CREATE OR REPLACE DATABASE nyc_trip_db;
 USE DATABASE nyc_trip_db;
@@ -39,4 +13,3 @@ CREATE OR REPLACE STAGE nyc_trip_stage;
 CREATE OR REPLACE SCHEMA bronze;
 CREATE OR REPLACE SCHEMA silver;
 CREATE OR REPLACE SCHEMA gold;
->>>>>>> def9b2e (First commit)
